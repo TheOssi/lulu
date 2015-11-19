9bedbb074a3b77dcfb9b9db85ad01168d4baecf2
Kai Müller <kai.mueller01@sap.com>
2015-11-19 11:16:01 +0100

    Rename QuerManager Interface and implemented Class Implement the Answer Class Implement the User Class

--- src/com/betit/entities/User.java
+++ src/com/betit/entities/User.java
@@ -1,5 +1,50 @@
 package com.betit.entities;
 
+import java.util.Date;
+
 public class User {
 
+	private Long userID;
+	private final String phoneNrHash;
+	private final String username;
+	private final String userHash;
+	private final Date accessionDate;
+	private String profilePictureURI;
+	private final int globaleScore;
+
+	public User(final String phoneNrHash, final String username, final String userHash, final Date accessionDate, final int globaleScore) {
+		this.phoneNrHash = phoneNrHash;
+		this.username = username;
+		this.userHash = userHash;
+		this.accessionDate = accessionDate;
+		this.globaleScore = globaleScore;
+	}
+
+	public Long getUserID() {
+		return userID;
+	}
+
+	public String getPhoneNrHash() {
+		return phoneNrHash;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public String getUserHash() {
+		return userHash;
+	}
+
+	public Date getAccessionDate() {
+		return accessionDate;
+	}
+
+	public String getProfilePictureURI() {
+		return profilePictureURI;
+	}
+
+	public int getGlobaleScore() {
+		return globaleScore;
+	}
 }
