876dd8c58c097b54b9c2e4331d2856340509ccc8
Kai Müller <kai.mueller01@sap.com>
2015-11-20 16:30:07 +0100

    Implement Entities

--- src/com/betit/entities/Winner.java
+++ src/com/betit/entities/Winner.java
@@ -2,4 +2,19 @@ package com.betit.entities;
 
 public class Winner {
 
+	private final Long bqID;
+	private final Long userID;
+
+	public Winner(final Long bqID, final Long userID) {
+		this.bqID = bqID;
+		this.userID = userID;
+	}
+
+	public Long getBqID() {
+		return bqID;
+	}
+
+	public Long getUserID() {
+		return userID;
+	}
 }
