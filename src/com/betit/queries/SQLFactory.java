43c64eb1a37524499200ff3485044793cd97c2dc
Kai Müller <kai.mueller01@sap.com>
2015-11-22 15:39:02 +0100

    Add some methods to the QueryManager

--- src/com/betit/queries/SQLFactory.java
+++ src/com/betit/queries/SQLFactory.java
@@ -39,4 +39,73 @@ public class SQLFactory {
 		statement.append(" WHERE ");
 		return statement.toString();
 	}
+
+	/**
+	 * This method build the start of a INSERT Statement for all columns with
+	 * the keyword "VALUES (", but without the values
+	 * 
+	 * @param schema
+	 *            Schema name
+	 * @param table
+	 *            Table name
+	 * @return a INSERT Stament without the values
+	 */
+	public static String buildSimpleInsertStatement(final String schema, final String table) {
+		final StringBuilder statement = new StringBuilder();
+		statement.append("INSERT INTO ");
+		statement.append(schema);
+		statement.append(".");
+		statement.append(table);
+		statement.append(" VALUES ( ");
+		return statement.toString();
+	}
+
+	/**
+	 * This method build the start of a INSERT Statement for specified columns
+	 * with the keyword "VALUES (", but without the values
+	 * 
+	 * @param schema
+	 *            Schema name
+	 * @param table
+	 *            Table name
+	 * @param columns
+	 *            the columns in witch the values shoud be inserted
+	 * @return a INSERT Stament without the values
+	 */
+	public static String buildInsertStatement(final String schema, final String table, final String[] columns) {
+		final StringBuilder statement = new StringBuilder();
+		statement.append("INSERT INTO ");
+		statement.append(schema);
+		statement.append(".");
+		statement.append(table);
+		statement.append("(");
+		for (final String column : columns) {
+			statement.append(column);
+			statement.append(",");
+		}
+		statement.delete(statement.length() - 2, statement.length() - 1);
+		statement.append(")");
+		statement.append(" VALUES ( ");
+		return statement.toString();
+	}
+
+	/**
+	 * This method build the start of a DELETE Statement with the keyword
+	 * "WHERE", but without a condition
+	 * 
+	 * @param schema
+	 *            Schema name
+	 * @param table
+	 *            Table name
+	 * @return a DELETE Stament without the WHERE conditions
+	 */
+	public static String buildDeleteStatement(final String schema, final String table) {
+		final StringBuilder statement = new StringBuilder();
+		statement.append("DELETE FROM ");
+		statement.append(schema);
+		statement.append(".");
+		statement.append(table);
+		statement.append(" WHERE ");
+		return statement.toString();
+	}
 }
