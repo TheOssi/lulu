b37a31f0dfbe01605d07113d5120300321d81959
Kai Müller <kai.mueller01@sap.com>
2015-11-23 14:19:46 +0100

    Update Entities

--- src/com/betit/entities/Question.java
+++ src/com/betit/entities/Question.java
@@ -2,15 +2,13 @@ package com.betit.entities;
 
 import java.util.Date;
 
-public class BQ {
+public class Question {
 
-	private final Long bqID;
-	private final String title;
+	private final Long questionID;
 	private final String question;
 	private final String additionalInformation;
 	private final Long hostID;
 	private final Long groupID;
-	private final String pictureURI;
 	private final Date createDate;
 	private final Date endDate;
 	private final Boolean allAnswered;
@@ -19,20 +17,15 @@ public class BQ {
 	private final Integer sumOfUsersToAnswer;
 	private final Boolean finished;
 	private final Long rightAnswerID;
-	private final Boolean doubed;
-	private final Boolean isBet;
 
-	public BQ(final Long bqID, final String title, final String question, final String additionalInformation, final Long hostID, final Long groupID,
-			final String pictureURI, final Date createDate, final Date endDate, final Boolean allAnswered, final Boolean optionExtension,
-			final Integer definitionOfEnd, final Integer sumOfUsersToAnswer, final Boolean finished, final Long rightAnswerID, final Boolean doubed,
-			final Boolean isBet) {
-		this.bqID = bqID;
-		this.title = title;
+	public Question(final Long questionID, final String question, final String additionalInformation, final Long hostID, final Long groupID,
+			final Date createDate, final Date endDate, final Boolean allAnswered, final Boolean optionExtension, final Integer definitionOfEnd,
+			final Integer sumOfUsersToAnswer, final Boolean finished, final Long rightAnswerID) {
+		this.questionID = questionID;
 		this.question = question;
 		this.additionalInformation = additionalInformation;
 		this.hostID = hostID;
 		this.groupID = groupID;
-		this.pictureURI = pictureURI;
 		this.createDate = createDate;
 		this.endDate = endDate;
 		this.allAnswered = allAnswered;
@@ -41,16 +34,14 @@ public class BQ {
 		this.sumOfUsersToAnswer = sumOfUsersToAnswer;
 		this.finished = finished;
 		this.rightAnswerID = rightAnswerID;
-		this.doubed = doubed;
-		this.isBet = isBet;
 	}
 
 	public Long getBqID() {
-		return bqID;
+		return questionID;
 	}
 
 	public String getTitle() {
-		return title;
+		return question;
 	}
 
 	public String getQuestion() {
@@ -67,10 +58,6 @@ public class BQ {
 
 	public Long getGroupID() {
 		return groupID;
-	}
-
-	public String getPictureURI() {
-		return pictureURI;
 	}
 
 	public Date getCreateDate() {
@@ -104,13 +91,4 @@ public class BQ {
 	public Long getRightAnswerID() {
 		return rightAnswerID;
 	}
-
-	public Boolean getDoubed() {
-		return doubed;
-	}
-
-	public Boolean getIsBet() {
-		return isBet;
-	}
-
 }
