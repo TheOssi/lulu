6863773e7a39cd9c925db6e841c7d2e7b3d096f1
TheOssi <lenk.max@googlemail.com>
2015-11-24 10:00:53 +0100

    Edited SessionManager, new Exception

--- src/com/betit/errorhandling/Error.java
+++ src/com/betit/errorhandling/Error.java
@@ -1,29 +1,12 @@
 package com.betit.errorhandling;
 
 public class Error {
-	ErrorCode errorCode;
 	String message;
 	String metadata;
 
-	public Error(ErrorCode msg, String metadata) {
-		this.errorCode = msg;
-		this.message = msg.toString();
-		this.metadata = metadata;
+	public Error(String metadata) {
+		
 	}
 
-	public ErrorCode getMsg() {
-		return errorCode;
-	}
-
-	public void setMsg(ErrorCode msg) {
-		this.errorCode = msg;
-	}
-
-	public String getMetadata() {
-		return metadata;
-	}
-
-	public void setMetadata(String metadata) {
-		this.metadata = metadata;
-	}
+	
 }
