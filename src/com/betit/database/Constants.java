8f4ea7c65bbea4ff41036a24b5c2e3cad0f0eeb9
Kai Müller <kai.mueller01@sap.com>
2015-11-24 08:56:25 +0100

    Add Test Database to Constants Implment some test for the session Manager

--- src/com/betit/database/Constants.java
+++ src/com/betit/database/Constants.java
@@ -3,6 +3,7 @@ package com.betit.database;
 public class Constants {
 
 	public static final String SCHEMA_NAME = "APP";
+	public final static String TEST_SCHEMA_NAME = "APP_TEST";
 	public static final String TABLE_BQS = "Questions";
 	public static final String TABLE_USERS = "Users";
 
--- src/com/betit/tests/CheckSessionManager.java
+++ src/com/betit/tests/CheckSessionManager.java
@@ -2,19 +2,31 @@ package com.betit.tests;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import com.betit.database.ConnectionFactory;
+import com.betit.database.Constants;
+import com.betit.exception.DriverNotFoundException;
 import com.betit.face.SessionManager;
+import com.betit.queries.SQLFactory;
 
 public class CheckSessionManager {
+	private static String[] hashes = new String[] { "ABCDEFG", "BCDEFGH", "CDEFGHI", "DEFGHIJ" };
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
-		// TODO insert Users
+		for (final String hash : hashes) {
+			registerUserInDatabase(hash);
+		}
 	}
 
 	@AfterClass
@@ -24,10 +36,12 @@ public class CheckSessionManager {
 
 	@Before
 	public void setUp() throws Exception {
+		SessionManager.getInstance();
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		SessionManager.destroy();
 	}
 
 	@Test
@@ -37,7 +51,8 @@ public class CheckSessionManager {
 
 	@Test
 	public void testCreateSessionSimple() {
-
+		SessionManager.getInstance().createSession(hashes[0]);
+		// TODO test an sich
 	}
 
 	@Test
@@ -46,13 +61,29 @@ public class CheckSessionManager {
 	}
 
 	@Test
-	public void testCreateSessionWithFalseHash() {
+	public void testCreateSessionWithNullHash() {
+		SessionManager.getInstance().createSession(null);
+	}
 
+	@Test
+	public void testCreateSessionWithFalseHash() {
+		SessionManager.getInstance().createSession("THISISNOTAHASH");
 	}
 
 	@Test
 	public void testTimeout() {
 
+	}
+
+	private static void registerUserInDatabase(final String hash) throws SQLException, DriverNotFoundException {
+		final Connection writerConnection = ConnectionFactory.getInstance().getWriterConnection();
+		final String[] columns = new String[] { "phoneNumberHash", "username", "accessionDate" };
+		final String firstPart = SQLFactory.buildInsertStatement(Constants.TEST_SCHEMA_NAME, Constants.TABLE_USERS, columns);
+		final PreparedStatement statement = writerConnection.prepareStatement(firstPart + "?,?,? );");
+		statement.setString(1, hash);
+		statement.setString(2, "TestUser");
+		statement.setDate(3, new Date(System.currentTimeMillis()));
+		statement.executeQuery();
 	}
 
 }
