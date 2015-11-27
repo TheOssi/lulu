8e0466c647f6e9219d9c781b9f1b907c3eb8f9c0
Max Lenk <max.lenk@sap.com>
2015-11-27 14:41:45 +0100

    Updated JsonBuilder + TestClass

--- src/com/askit/face/JSONBuilder.java
+++ src/com/askit/face/JSONBuilder.java
@@ -1,11 +1,20 @@
 package com.askit.face;
 
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Map;
+
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import com.google.gson.reflect.TypeToken;
 
 public class JSONBuilder {
-	Gson gson = new Gson();
-
+	GsonBuilder gb = new GsonBuilder();
+	Gson gson = 	gb.setPrettyPrinting().create();
+	
 	public JSONBuilder() {
 
 	}
@@ -13,22 +22,39 @@ public class JSONBuilder {
 	// Generate from Single Entity
 	public String createJSON(final Object o) {
 
-		final JsonObject jo = new JsonObject();
-		final String json = gson.toJson(o);
-
-		jo.addProperty(o.getClass().getSimpleName(), json);
+		final JsonObject jo = new JsonObject(); 
+	
+		jo.add(o.getClass().getSimpleName(), gson.toJsonTree(o));
 		return gson.toJson(jo);
-
 	}
 
 	// Generate from Collection
 	public String createJSON(final Object[] objectArray) {
 		final JsonObject jo = new JsonObject();
-		String json;
-		for (final Object currentObject : objectArray) {
-			json = gson.toJson(currentObject);
-			jo.addProperty(currentObject.getClass().getSimpleName() + currentObject.hashCode(), json);
+		final JsonObject innerJo = new JsonObject();
+		for (Object currentObject : objectArray) {
+			innerJo.add(currentObject.getClass().getSimpleName() + currentObject.hashCode(), gson.toJsonTree(currentObject));
+			jo.add(currentObject.getClass().getSimpleName() + "Set", innerJo);
 		}
 		return gson.toJson(jo);
+	}
+	
+	private String removeNulls(String json){
+		Type type = new TypeToken<Map<String, Object>>() {}.getType();
+		Map<String, Object> data = new Gson().fromJson(json, type);
+
+		for (Iterator<Map.Entry<String, Object>> it = data.entrySet().iterator(); it.hasNext();) {
+		    Map.Entry<String, Object> entry = it.next();
+		    if (entry.getValue() == null) {
+		        it.remove();
+		    } else if (entry.getValue().getClass().equals(ArrayList.class)) {
+		        if (((ArrayList<?>) entry.getValue()).size() == 0) {
+		            it.remove();
+		        }
+		    }
+		}
+
+		json = new GsonBuilder().setPrettyPrinting().create().toJson(data);
+		return json;
 	}
 }
