1f0cac8ce6fd3236f3510e839b90b1e59aa961c6
Kai Müller <kai.mueller01@sap.com>
2015-11-27 15:51:57 +0100

    Tests

--- src/com/askit/entities/Answer.java
+++ src/com/askit/entities/Answer.java
@@ -3,16 +3,16 @@ package com.askit.entities;
 public class Answer {
 
 	private final Long answerID;
-	private final Long bqID;
+	private final Long questionID;
 	private final String answer;
 
-	public Answer(final long bqID, final String answer) {
-		this(null, bqID, answer);
+	public Answer(final long questionID, final String answer) {
+		this(null, questionID, answer);
 	}
 
-	public Answer(final Long answerID, final Long bqID, final String answer) {
+	public Answer(final Long answerID, final Long questionID, final String answer) {
 		this.answerID = answerID;
-		this.bqID = bqID;
+		this.questionID = questionID;
 		this.answer = answer;
 	}
 
@@ -20,8 +20,8 @@ public class Answer {
 		return answerID;
 	}
 
-	public Long getBqID() {
-		return bqID;
+	public Long getQuestionID() {
+		return questionID;
 	}
 
 	public String getAnswer() {
--- tests/com/askit/JSONBuilderTest.java
+++ tests/com/askit/JSONBuilderTest.java
@@ -1,5 +1,7 @@
 package com.askit;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+
 import org.junit.Test;
 
 import com.askit.entities.Answer;
@@ -8,30 +10,42 @@ import com.askit.face.JSONBuilder;
 
 public class JSONBuilderTest {
 	@Test
-	public void testBuildOneEnitity(){
-		Answer aw = new Answer(new Long(2325), "Hello");
-		JSONBuilder  jb = new JSONBuilder();
-		System.out.println(jb.createJSON(aw));
+	public void testBuildOneEnitity() {
+		final Answer answer = new Answer(new Long(2325), "Hello");
+		final JSONBuilder jsonBuilder = new JSONBuilder();
+		final String jsonString = normalizeJSONString(jsonBuilder.createJSON(answer));
+		final String jsonExpected = "{\"Answer\":{\"questionID\":" + answer.getQuestionID().intValue() + ",\"answer\":\"" + answer.getAnswer()
+				+ "\"}}";
+		assertThat("The json doens't match the expected JSON", jsonExpected.equals(jsonString));
+
 	}
+
 	@Test
-	public void testBuildEntitiySet(){
-		Answer aw = new Answer(new Long(2325), "Hello");
-		Answer aw2 = new Answer(new Long(232422), "uhuhu");
-		Answer aw3 = new Answer(new Long(232423), "Hi");
-		Answer aw4 = new Answer(new Long(232424), "Answer");
-		JSONBuilder  jb = new JSONBuilder();
-		Answer[] aws = new Answer[5];
-		aws[0] = aw;
-		aws[1] = aw;
-		aws[2] = aw3;
-		aws[3] = aw4;
-		aws[4] = aw2;
-		System.out.println(jb.createJSON(aws));
+	public void testBuildEntitiySet() {
+		final String[] answerStrings = new String[] { "Hello", "uhuhu", "Hi", "Answer" };
+		final Long[] ids = new Long[] { 2325L, 232422L, 232423L, 232424L };
+		final Answer[] answers = new Answer[4];
+		for (int i = 0; i < answers.length; i++) {
+			answers[i] = new Answer(ids[i], answerStrings[i]);
+		}
+		final String jsonString = normalizeJSONString(new JSONBuilder().createJSON(answers));
+
+		// String jsonExpected = "{\"AnswerSet\":{";
+		// for (final Answer answer : answers) {
+		// jsonExpected = jsonExpected + "\"" + Answer.class.getSimpleName() +
+		// answer.hashCode()
+		// }
+
 	}
+
 	@Test
-	public void testRemoveNull(){
-	Message m = new Message(new Long(0), new Long(2324), null, null);
-	JSONBuilder  jb = new JSONBuilder();
-	System.out.println(jb.createJSON(m));
+	public void testRemoveNull() {
+		final Message m = new Message(new Long(0), new Long(2324), null, null);
+		final JSONBuilder jb = new JSONBuilder();
+		System.out.println(jb.createJSON(m));
+	}
+
+	private String normalizeJSONString(final String json) {
+		return json.trim().replace("\t", "").replace(" ", "").replace(System.lineSeparator(), "").replace("\n", "");
 	}
 }
