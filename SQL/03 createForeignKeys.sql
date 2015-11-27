612c9c5428eddb070aa016be6b91aed4bffc6fe9
Kai Müller <kai.mueller01@sap.com>
2015-11-27 14:44:15 +0100

    Update SQL, Update Modell, UPdate Nötige Methoden

--- SQL/03 createForeignKeys.sql
+++ SQL/03 createForeignKeys.sql
@@ -1,72 +1,39 @@
 USE APP;
 
-ALTER TABLE Questions
-	ADD CONSTRAINT fk_questions_groupID
+ALTER TABLE PrivateQuestions
+	ADD CONSTRAINT fk_PrivateQuestions_groupID
 		FOREIGN KEY ( groupID ) REFERENCES Groups( groupID )
-			ON DELETE NO ACTION
+			ON DELETE CASCADE
 			ON UPDATE NO ACTION,
-	ADD CONSTRAINT fk_questions_hostID
+	ADD CONSTRAINT fk_PrivateQuestions_hostID
 		FOREIGN KEY ( hostID ) REFERENCES Users( userID )
 			ON DELETE SET NULL
 			ON UPDATE NO ACTION,
-	ADD CONSTRAINT fk_questions_rightAnswerID
+	ADD CONSTRAINT fk_PrivateQuestions_selectedAnswerID
 		FOREIGN KEY ( rightAnswerID ) REFERENCES Answers ( answerID )
 			ON DELETE NO ACTION
 			ON UPDATE NO ACTION;
-
-
-ALTER TABLE Groups
-	ADD CONSTRAINT fk_Groups_adminID
-		FOREIGN KEY ( adminID ) REFERENCES Users ( userID )
-			ON DELETE NO ACTION
-			ON UPDATE NO ACTION;
-	
-
-ALTER TABLE Answers
-	ADD CONSTRAINT fk_Answers_questionID
-		FOREIGN KEY ( questionID ) REFERENCES Questions ( questionID )
+			
+ALTER TABLE AnswersOfPrivateQuestions
+	ADD CONSTRAINT fk_AnswersOfPrivateQuestions_questionID
+		FOREIGN KEY ( questionID ) REFERENCES PrivateQuestions ( questionID )
 			ON DELETE CASCADE
 			ON UPDATE NO ACTION;
 			
-			
-ALTER TABLE Messages
-	ADD CONSTRAINT fk_Messages_groupID
-		FOREIGN KEY ( groupID ) REFERENCES Groups( groupID )
-			ON DELETE NO ACTION
+ALTER TABLE PrivateQuestionsToUsers
+	ADD CONSTRAINT fk_PrivateQuestionsToUsers_questionID
+		FOREIGN KEY ( questionID ) REFERENCES PrivateQuestions( questionID )
+			ON DELETE CASCADE
 			ON UPDATE NO ACTION,
-	ADD CONSTRAINT fk_Messages_userID
+	ADD CONSTRAINT fk_PrivateQuestionsToUsers_userID
 		FOREIGN KEY ( userID ) REFERENCES Users( userID )
+			ON DELETE CASCADE
+			ON UPDATE NO ACTION,
+	ADD CONSTRAINT fk_PrivateQuestionsToUsers_choosedAnswerID
+		FOREIGN KEY ( choosedAnswerID ) REFERENCES Answers( answerID )
 			ON DELETE SET NULL
 			ON UPDATE NO ACTION;
-	
-
-
-ALTER TABLE QuestionsToUsers
-	ADD CONSTRAINT fk_questionsToUsers_questionID
-		FOREIGN KEY ( questionID ) REFERENCES Questions( questionID )
-			ON DELETE CASCADE
-			ON UPDATE NO ACTION,
-	ADD CONSTRAINT fk_questionsToUsers_userID
-		FOREIGN KEY ( userID ) REFERENCES Users( userID )
-			ON DELETE CASCADE
-			ON UPDATE NO ACTION,
-	ADD CONSTRAINT fk_questionsToUsers_choosedAnswerID
-		FOREIGN KEY ( choosedAnswerID ) REFERENCES Answers( answerID )
-			ON DELETE CASCADE
-			ON UPDATE NO ACTION;
-
-
-
-ALTER TABLE GroupsToUsers
-	ADD CONSTRAINT fk_GroupsToUsers_groupID
-		FOREIGN KEY ( groupID ) REFERENCES Groups ( groupID )
-			ON DELETE NO ACTION
-			ON UPDATE NO ACTION,
-	ADD CONSTRAINT fk_GroupsToUsers_userID
-		FOREIGN KEY ( userID ) REFERENCES Users ( userID )
-			ON DELETE CASCADE
-			ON UPDATE NO ACTION;
-
+			
 ALTER TABLE Contacts
 	ADD CONSTRAINT fk_Contacs_userID
 		FOREIGN KEY ( groupID ) REFERENCES Users ( userID )
@@ -76,8 +43,35 @@ ALTER TABLE Contacts
 		FOREIGN KEY ( userID ) REFERENCES Users ( userID )
 			ON DELETE CASCADE
 			ON UPDATE NO ACTION;
-								  
 			
+ALTER TABLE Messages
+	ADD CONSTRAINT fk_Messages_groupID
+		FOREIGN KEY ( groupID ) REFERENCES Groups( groupID )
+			ON DELETE CASCADE
+			ON UPDATE NO ACTION,
+	ADD CONSTRAINT fk_Messages_userID
+		FOREIGN KEY ( userID ) REFERENCES Users( userID )
+			ON DELETE SET NULL
+			ON UPDATE NO ACTION;
+
+ALTER TABLE Groups
+	ADD CONSTRAINT fk_Groups_adminID
+		FOREIGN KEY ( adminID ) REFERENCES Users ( userID )
+			ON DELETE NO ACTION
+			ON UPDATE NO ACTION;
+	
+ALTER TABLE GroupsToUsers
+	ADD CONSTRAINT fk_GroupsToUsers_groupID
+		FOREIGN KEY ( groupID ) REFERENCES Groups ( groupID )
+			ON DELETE CASCADE
+			ON UPDATE NO ACTION,
+	ADD CONSTRAINT fk_GroupsToUsers_userID
+		FOREIGN KEY ( userID ) REFERENCES Users ( userID )
+			ON DELETE CASCADE
+			ON UPDATE NO ACTION;
+
+-- Users no foreign Key
+								  		
 ALTER TABLE NotificationsToUsers
 	ADD CONSTRAINT fk_NotificationsToUsers_userID
 		FOREIGN KEY ( userID ) REFERENCES Users ( userID )
@@ -87,7 +81,30 @@ ALTER TABLE NotificationsToUsers
 		FOREIGN KEY ( notificationID ) REFERENCES Notifications ( notificationID )
 			ON DELETE CASCADE
 			ON UPDATE NO ACTION;
-									  
+			
+ALTER TABLE PublicQuestions
+	ADD CONSTRAINT fk_PublicQuestions_hostID
+		FOREIGN KEY ( hostID ) REFERENCES Users( userID )
+			ON DELETE SET NULL
+			ON UPDATE NO ACTION;
+			
+ALTER TABLE AnswersOfPublicQuestions
+	ADD CONSTRAINT fk_AnswersOfPublicQuestions_questionID
+		FOREIGN KEY ( questionID ) REFERENCES PublicQuestions ( questionID )
+			ON DELETE CASCADE
+			ON UPDATE NO ACTION;
+			
+ALTER TABLE PublicQuestionsToUsers
+	ADD CONSTRAINT fk_PublicQuestionsToUsers_questionID
+		FOREIGN KEY ( questionID ) REFERENCES PublicQuestions( questionID )
+			ON DELETE CASCADE
+			ON UPDATE NO ACTION,
+	ADD CONSTRAINT fk_PublicQuestionsToUsers_userID
+		FOREIGN KEY ( userID ) REFERENCES Users( userID )
+			ON DELETE CASCADE
+			ON UPDATE NO ACTION;
+			
+-- Notification no foreign key
 									  
 ALTER TABLE NotificationsParameters
 	ADD CONSTRAINT fk_NotificatiosnParameters_notificationID
