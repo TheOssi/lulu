243d7fba0269f4f5d07d7797e14b826db2bc7986
Kai Müller <kai.mueller01@sap.com>
2016-04-18 10:18:28 +0200

    add new version of the connectionManager

--- src/main/java/com/askit/database/ConnectionManager.java
+++ src/main/java/com/askit/database/ConnectionManager.java
@@ -1,45 +1,79 @@
 package com.askit.database;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import org.apache.commons.dbcp2.BasicDataSource;
+//TODO drivernotfoundexception
 import com.askit.etc.Constants;
-import com.askit.exception.DriverNotFoundException;
 
 public class ConnectionManager {
-	private final static String JDBC_PROTOCOLL = "jdbc:mysql";
-	private final static String PARAMETER_USER = "user";
-	private final static String PARAMETER_PASSWORD = "password";
-	private static final String IP_OF_DATABASE = "localhost";
 	private static final String MARIA_DB_DRIVER = "org.mariadb.jdbc.Driver";
-	private static ConnectionManager instance;
+	private static final String IP_OF_DATABASE = "localhost";
+	private static final String JDBC_PROTOCOLL = "jdbc:mysql";
+	private static final String URL = JDBC_PROTOCOLL + "://" + IP_OF_DATABASE + "/" + Constants.SCHEMA_NAME;
+	private static final ConnectionManager INSTANCE = new ConnectionManager();
 
-	private Connection readerConnection;
-	private Connection writerConnection;
-	private Connection deleteConnection;
+	private BasicDataSource readerDataSource;
+	private BasicDataSource writerDataSource;
+	private BasicDataSource deleterDataSource;
 
-	private ConnectionManager() throws DriverNotFoundException {
-		try {
-			Class.forName(MARIA_DB_DRIVER);
-		} catch (final ClassNotFoundException e) {
-			e.printStackTrace();
-			throw new DriverNotFoundException("Driver class not found");
-		}
+	private ConnectionManager() {
+		setDeleterDataSource();
+		setReaderDataSource();
+		setWriterDataSource();
 	}
 
-	/**
-	 * This instance handles all connections to the database
-	 *
-	 * @return the instance of the ConnectionFactory
-	 * @throws DriverNotFoundException
-	 *             if the driver wasn't found
-	 */
-	public static synchronized ConnectionManager getInstance() throws DriverNotFoundException {
-		if (instance == null) {
-			instance = new ConnectionManager();
-		}
-		return instance;
+	private void setReaderDataSource() {
+		readerDataSource = new BasicDataSource();
+		readerDataSource.setDriverClassName(MARIA_DB_DRIVER);
+		readerDataSource.setUsername(DatabaseUser.READ_USER.getUsername());
+		readerDataSource.setPassword(DatabaseUser.READ_USER.getPassword());
+		readerDataSource.setUrl(URL);
+		readerDataSource.setDefaultAutoCommit(true);
+		readerDataSource.setDefaultReadOnly(true);
+
+		// TODO
+		readerDataSource.setMinIdle(20);
+		readerDataSource.setMaxIdle(25);
+		readerDataSource.setMaxTotal(-1);
+		readerDataSource.setMaxOpenPreparedStatements(180);
+	}
+
+	private void setWriterDataSource() {
+		writerDataSource = new BasicDataSource();
+		writerDataSource.setDriverClassName(MARIA_DB_DRIVER);
+		writerDataSource.setUsername(DatabaseUser.READ_USER.getUsername());
+		writerDataSource.setPassword(DatabaseUser.READ_USER.getPassword());
+		writerDataSource.setUrl(URL);
+		readerDataSource.setDefaultAutoCommit(true);
+		readerDataSource.setDefaultReadOnly(false);
+
+		// TODO
+		writerDataSource.setMinIdle(20);
+		writerDataSource.setMaxIdle(25);
+		writerDataSource.setMaxTotal(-1);
+		writerDataSource.setMaxOpenPreparedStatements(180);
+	}
+
+	private void setDeleterDataSource() {
+		deleterDataSource = new BasicDataSource();
+		deleterDataSource.setDriverClassName(MARIA_DB_DRIVER);
+		deleterDataSource.setUsername(DatabaseUser.READ_USER.getUsername());
+		deleterDataSource.setPassword(DatabaseUser.READ_USER.getPassword());
+		deleterDataSource.setUrl(URL);
+		readerDataSource.setDefaultAutoCommit(true);
+		readerDataSource.setDefaultReadOnly(false);
+
+		// TODO
+		deleterDataSource.setMinIdle(20);
+		deleterDataSource.setMaxIdle(25);
+		deleterDataSource.setMaxTotal(-1);
+		deleterDataSource.setMaxOpenPreparedStatements(180);
+	}
+
+	public static synchronized ConnectionManager getInstance() {
+		return INSTANCE;
 	}
 
 	/**
@@ -48,13 +82,7 @@ public class ConnectionManager {
 	 * @throws SQLException
 	 */
 	public synchronized Connection getReaderConnection() throws SQLException {
-		if (readerConnection == null || readerConnection.isClosed()) {
-			readerConnection = DriverManager.getConnection(buildLoginUR(DatabaseUser.READ_USER));
-			readerConnection.setAutoCommit(true);
-			readerConnection.setReadOnly(true);
-		}
-		// TODO timeout
-		return readerConnection;
+		return readerDataSource.getConnection();
 	}
 
 	/**
@@ -63,12 +91,7 @@ public class ConnectionManager {
 	 * @throws SQLException
 	 */
 	public synchronized Connection getWriterConnection() throws SQLException {
-		if (writerConnection == null || writerConnection.isClosed()) {
-			writerConnection = DriverManager.getConnection(buildLoginUR(DatabaseUser.WRITE_USER));
-			writerConnection.setAutoCommit(true);
-		}
-		// TODO timeout
-		return writerConnection;
+		return writerDataSource.getConnection();
 	}
 
 	/**
@@ -77,32 +100,6 @@ public class ConnectionManager {
 	 * @throws SQLException
 	 */
 	public synchronized Connection getDeleteConnection() throws SQLException {
-		if (deleteConnection == null || deleteConnection.isClosed()) {
-			deleteConnection = DriverManager.getConnection(buildLoginUR(DatabaseUser.DELETE_USER));
-			deleteConnection.setAutoCommit(true);
-		}
-		// TODO timeout
-		return deleteConnection;
-	}
-
-	public Connection buildOneTimeConnection(final String username, final String password) throws SQLException {
-		final Connection connection = DriverManager.getConnection(buildLoginURL(username, password));
-		return connection;
-	}
-
-	/**
-	 *
-	 * @param databaseUser
-	 *            the password and the username of a database user
-	 * @return the login URL
-	 */
-	private String buildLoginUR(final DatabaseUser databaseUser) {
-		return buildLoginURL(databaseUser.getUsername(), databaseUser.getPassword());
-	}
-
-	private String buildLoginURL(final String username, final String password) {
-		final String mainPart = JDBC_PROTOCOLL + "://" + IP_OF_DATABASE + "/" + Constants.SCHEMA_NAME + "?";
-		final String parameterPart = PARAMETER_USER + "=" + username + "&" + PARAMETER_PASSWORD + "=" + password;
-		return mainPart + parameterPart;
+		return deleterDataSource.getConnection();
 	}
 }
