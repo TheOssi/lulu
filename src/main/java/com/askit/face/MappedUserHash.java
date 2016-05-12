47211fd6f4b344adbb9b43089fd43bc1245d290a
Kai <Kai@DESKTOP-RBETVED>
2016-05-13 00:23:56 +0200

    review & javadoc

--- src/main/java/com/askit/face/MappedUserHash.java
+++ src/main/java/com/askit/face/MappedUserHash.java
@@ -4,16 +4,16 @@ public class MappedUserHash {
 	private final String username;
 	private final long time;
 
+	public MappedUserHash(final String username, final long time) {
+		this.username = username;
+		this.time = time;
+	}
+
 	public String getUsername() {
 		return username;
 	}
 
 	public long getTime() {
 		return time;
-	}
-
-	public MappedUserHash(final String username, final long time) {
-		this.username = username;
-		this.time = time;
 	}
 }
