34f870a659d07757a5fd4973b1375abbd6ff9c1a
Kai Müller <kai.mueller01@sap.com>
2016-05-12 14:28:40 +0200

    javadoc and review

--- src/main/java/com/askit/notification/NotificationSupporter.java
+++ src/main/java/com/askit/notification/NotificationSupporter.java
@@ -8,68 +8,38 @@ import com.askit.exception.NotificationException;
 
 public class NotificationSupporter {
 
-	public NotificationSupporter() {
+	private static final QueryManager QUERY_MANAGER = new DatabaseQueryManager();
 
+	public static void sendNotificationToAllMembersOfAGroup(final Notification notification, final Long groupID) throws DatabaseLayerException,
+			NotificationException {
+		final User[] users = QUERY_MANAGER.getUsersOfGroup(groupID);
+		sendNotificationToUsers(notification, users);
 	}
 
-	public static void sendNotificationToAllMembersOfAGroup(final Notification not, final Long groupID) throws DatabaseLayerException, NotificationException {
-		final QueryManager queryManager = new DatabaseQueryManager();
-		final User[] users = queryManager.getUsersOfGroup(groupID);
-		final RegIDHandler regIDHandler = RegIDHandler.getInstance();
-		final NotificationHandler notificationHandler = NotificationHandler.getInstance();
+	public static void sendNotificationToAllMembersOfPrivateQuestion(final Notification notification, final Long questionID)
+			throws DatabaseLayerException, NotificationException {
+		final User[] users = QUERY_MANAGER.getUsersOfPrivateQuestion(questionID);
+		sendNotificationToUsers(notification, users);
+	}
+
+	public static void sendNotificationToAllMembersOfPublicQuestion(final Notification notification, final Long questionID)
+			throws DatabaseLayerException, NotificationException {
+		final User[] users = QUERY_MANAGER.getUsersOfPublicQuestion(questionID);
+		sendNotificationToUsers(notification, users);
+	}
+
+	private static void sendNotificationToUsers(final Notification notification, final User[] users) throws NotificationException {
 		for (final User user : users) {
-			if (not.getTo() == null) {
+			if (notification.getTo() == null) {
 				final Long userID = user.getUserID();
-				final String regID = regIDHandler.getRegIDFromUser(userID);
+				final String regID = RegIDHandler.getInstance().getRegIDFromUser(userID);
 				if (regID != null) {
-					not.setTo(regID);
+					notification.setTo(regID);
 				} else {
 					throw new NotificationException("No regID for User: " + userID);
 				}
-
 			}
-			notificationHandler.addNotification(not);
+			NotificationHandler.getInstance().addNotification(notification);
 		}
-	}
-
-	public static void sendNotificationToAllMembersOfPrivateQuestion(Notification not, Long questionID) throws DatabaseLayerException, NotificationException {
-		final QueryManager queryManager = new DatabaseQueryManager();
-		final User[] users = queryManager.getUsersOfPrivateQuestion(questionID);
-		final RegIDHandler regIDHandler = RegIDHandler.getInstance();
-		final NotificationHandler notificationHandler = NotificationHandler.getInstance();
-		for (final User user : users) {
-			if (not.getTo() == null) {
-				final Long userID = user.getUserID();
-				final String regID = regIDHandler.getRegIDFromUser(userID);
-				if (regID != null) {
-					not.setTo(regID);
-				} else {
-					throw new NotificationException("No regID for User: " + userID);
-				}
-
-			}
-			notificationHandler.addNotification(not);
-		}
-		
-	}
-	public static void sendNotificationToAllMembersOfPublicQuestion(Notification not, Long questionID) throws DatabaseLayerException, NotificationException {
-		final QueryManager queryManager = new DatabaseQueryManager();
-		final User[] users = queryManager.getUsersOfPublicQuestion(questionID);
-		final RegIDHandler regIDHandler = RegIDHandler.getInstance();
-		final NotificationHandler notificationHandler = NotificationHandler.getInstance();
-		for (final User user : users) {
-			if (not.getTo() == null) {
-				final Long userID = user.getUserID();
-				final String regID = regIDHandler.getRegIDFromUser(userID);
-				if (regID != null) {
-					not.setTo(regID);
-				} else {
-					throw new NotificationException("No regID for User: " + userID);
-				}
-
-			}
-			notificationHandler.addNotification(not);
-		}
-		
 	}
 }
