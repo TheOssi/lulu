c245bb3559c2d5615678b87c2ad193b77f74518a
Kai <Kai@DESKTOP-RBETVED>
2016-05-14 12:39:38 +0200

    working on questionSoonTime...; modify init; add todo

--- src/main/java/com/askit/face/Faceservlet.java
+++ src/main/java/com/askit/face/Faceservlet.java
@@ -14,6 +14,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.askit.database.DatabaseUser;
+import com.askit.database.QuestionEndTimeChecker;
 import com.askit.database.QuestionSoonEndTimeChecker;
 import com.askit.exception.DatabaseLayerException;
 import com.askit.exception.DuplicateHashException;
@@ -61,8 +62,9 @@ public class Faceservlet extends HttpServlet {
 			e.printStackTrace();
 		}
 		SessionManager.getInstance().start();
-		//NotificationSender.getInstace().startThread();
-		QuestionSoonEndTimeChecker.getInstance();
+		// NotificationSender.getInstace().startThread();
+		QuestionEndTimeChecker.getInstance().startThread();
+		QuestionSoonEndTimeChecker.getInstance().startThread();
 		RegIDHandler.getInstance();
 		NotificationHandler.getInstance();
 	}
@@ -94,8 +96,7 @@ public class Faceservlet extends HttpServlet {
 	}
 
 	@Override
-	protected void doPut(final HttpServletRequest request, final HttpServletResponse response)
-			throws ServletException, IOException {
+	protected void doPut(final HttpServletRequest request, final HttpServletResponse response) throws ServletException, IOException {
 		final PrintWriter out = getPrintWriterSlienty(response);
 		final PutRequest put = new PutRequest(request.getPathInfo(), request.getParameterMap(), out);
 		handleRequest(put, response, out);
@@ -103,8 +104,7 @@ public class Faceservlet extends HttpServlet {
 	}
 
 	@Override
-	protected void doDelete(final HttpServletRequest request, final HttpServletResponse response)
-			throws ServletException, IOException {
+	protected void doDelete(final HttpServletRequest request, final HttpServletResponse response) throws ServletException, IOException {
 		final PrintWriter out = getPrintWriterSlienty(response);
 		final DeleteRequest delete = new DeleteRequest(request.getPathInfo(), request.getParameterMap(), out);
 		handleRequest(delete, response, out);
@@ -143,8 +143,8 @@ public class Faceservlet extends HttpServlet {
 	private void handleRequest(final Request request, final HttpServletResponse response, final PrintWriter out) {
 		try {
 			request.handleRequest();
-		} catch (final ServletException | MissingParametersException | DatabaseLayerException | WrongHashException
-				| DuplicateHashException | NotificationException e) {
+		} catch (final ServletException | MissingParametersException | DatabaseLayerException | WrongHashException | DuplicateHashException
+				| NotificationException e) {
 			handleException(e, response, out);
 		}
 	}
