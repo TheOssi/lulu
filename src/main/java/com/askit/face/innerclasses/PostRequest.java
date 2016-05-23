75c0637d7dfb9c216f4a5aad18d982b1ccfbb826
TheOssi <lenk.max@googlemail.com>
2016-05-23 14:37:49 +0200

    small change

--- src/main/java/com/askit/face/innerclasses/PostRequest.java
+++ src/main/java/com/askit/face/innerclasses/PostRequest.java
@@ -316,7 +316,7 @@ public class PostRequest extends Request {
 			} else {
 				throw new MissingParametersException();
 			}
-
+			return;
 		}
 		matcher = regExPicturePattern.matcher(pathInfo);
 		if (matcher.find()) {
@@ -329,11 +329,13 @@ public class PostRequest extends Request {
 					PictureSupporter.createPictureFile(body, "/publicQuestion", userID);
 				} else if (questionID != null && !isPublic) {
 					PictureSupporter.createPictureFile(body, "/privateQuestion", userID);
+				}else{
+					throw new MissingParametersException("Missing ID for Entity");
 				}
 			} else {
 				throw new MissingParametersException("Empty or no Body");
 			}
-
+			return;
 		}
 		throw new ServletException();
 	}
