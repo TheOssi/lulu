57d7b59fd9a0acda0d5fddbb67e410c25b669168
Kai <Kai@PC-KAI>
2016-05-27 12:23:37 +0200

    simple bug fix (I hope so)

--- src/main/java/com/askit/database/ConnectionManager.java
+++ src/main/java/com/askit/database/ConnectionManager.java
@@ -8,39 +8,39 @@ import org.apache.commons.dbcp2.BasicDataSource;
 import com.askit.database.sqlHelper.Constants;
 
 /**
- * 
+ *
  * This manager provides all connections to the database. For that
  * functionallity a extern libary for pooling the connections is used. <br/>
  * Three types with different permissions are avaible:
  * <ul>
  * <li>Reader connection: This connection have only read permissions to all
  * tables necessary of the app</li>
- * 
+ *
  * <li>Writer connection: This connection have read and writer permissions to
  * all tables necessary of the app</li>
- * 
+ *
  * <li>Deleter connection: This connection have only delete permissions to some
  * tables necessary of the app</li>
  * </ul>
- * 
+ *
  * Also some attributes like timeouts, driver, ip, pool size are set to the
  * connections.
- * 
+ *
  * The pooling has a huge impact to the performance of the system. The pool
  * provides lazy connections. If somebody call a getConnection method, a
  * connection from the pool return. The caller have the duty to close the
  * connection after using it. Thus return the connection to the pool. If no
  * connection is avaible in the pool, the caller must wait.
- * 
+ *
  * @author Kai Müller
  * @since 1.0.0.0
  * @version 1.0.0.0
- * 
+ *
  */
 public class ConnectionManager {
 	private static final int MAX_IDLE = 0;
 	private static final int MIN_IDLE = 0;
-	private static final int MAX_PARALLEL_CONNECTIONS = 100;
+	private static final int MAX_PARALLEL_CONNECTIONS = 50;
 	private static final int INITIAL_IDLE_CONNECTIONS = 5;
 	private static final int MAX_PARALLEL_PREPARED_STATEMENTS = 100;
 	private static final int QUERY_TIMEOUT = 10000;
@@ -102,7 +102,7 @@ public class ConnectionManager {
 	 * Because only one instance of a manager should exist and handle all
 	 * connections to the database, this class is implemented as a singelton. So
 	 * this method retuns the only instance of this class.
-	 * 
+	 *
 	 * @return the only instance of the class ConnectionManager
 	 */
 	public static synchronized ConnectionManager getInstance() {
